0003398a0827aa621f1f76e120beb0f91ae88445
Jason K. Moore <moorepants@gmail.com>
2015-10-13 17:15:20 -0800

    Update documentation

--- static/mail/contact_me.php
+++ static/mail/contact_me.php
@@ -16,11 +16,11 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'jkm@ucdavis.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'jasonmoore@ucdavis.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "[MECH-CAP Proposal]:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@ucdavis.edu\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$headers = "From: noreply@moorepants.info\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
