00491503a2ff872c8fbb1d870f196ec2774c0ddf
Jason K. Moore <moorepants@gmail.com>
2016-10-10 22:55:40 -0700

    Form now fully works.

--- theme/static/js/contact_me.js
+++ theme/static/js/contact_me.js
@@ -50,7 +50,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your proposal has been sent and you should receive a confirmation email. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
--- theme/static/mail/contact_me.php
+++ theme/static/mail/contact_me.php
@@ -37,7 +37,7 @@ $size_of_uploaded_file =
 
 //Settings
 $max_allowed_file_size = 5000; // size in KB
-$allowed_extensions = array("txt", "rst", "md", "pdf", "odt", "doc", "docx");
+$allowed_extensions = array("txt", "rst", "md", "pdf", "odt", "doc", "docx", "jpg", "gif", "png");
 
 $errors = "";
 
@@ -68,7 +68,10 @@ $upload_folder = "/home/jasonkmoore/moorepants.info/eme185-uploads/";
 if (!file_exists($upload_folder)) {
     mkdir($upload_folder, 0777, true);
 }
-$path_of_uploaded_file = $upload_folder . $name_of_uploaded_file;
+$custom_file_name = mb_ereg_replace("([^\w\d\-_\[\]\(\)])", '_',
+  $name."-".$org."-".$title);
+$attachment_filename = $custom_file_name.'.'.$type_of_uploaded_file;
+$path_of_uploaded_file = $upload_folder . $attachment_filename;
 $tmp_path = $_FILES["file"]["tmp_name"];
 
 if(is_uploaded_file($tmp_path))
@@ -77,43 +80,100 @@ if(is_uploaded_file($tmp_path))
   {
     $errors .= '\n error while copying the uploaded file';
   }
+} else {
+  $attachment_filename = "";
 }
+
+// build csv file
+
+$csv_file = $upload_folder . 'proposals.csv';
+
+if (file_exists($csv_file)) {
+  $file = new SplFileObject($csv_file, 'a');
+} else {
+  $file = new SplFileObject($csv_file, 'w');
+}
+$file->fputcsv(array(
+  $name,
+  $org,
+  $email_address,
+  $phone,
+  $title,
+  preg_replace("/[\n\r]/", '\n', $message),
+  $attachment_filename
+));
+$file = null;
 
 // Create the email and send the message
 $to = 'jkm@ucdavis.edu';
+$to_name = "Jason K. Moore";
+$from = 'noreply@ucdavis.edu';
+$from_name = 'MECH-CAP Website';
 $email_subject = "[MECH-CAP Proposal]:  $name, $org";
 $email_body = <<<EOT
-  You have received a new message from your website contact form.\n\n
-  Here are the details:\n\n
-  Name: $name\n\n
-  Organization: $org\n\n
-  Email: $email_address\n\n
-  Phone: $phone\n\n
-  Title: $title\n\n
-  Message:\n$message
-  Attachment:\n$name_of_uploaded_file
+You have received a new message from your website contact form.\r\n\r\n
+Here are the details:\r\n\r\n
+Name: $name\r\n\r\n
+Organization: $org\r\n\r\n
+Email: $email_address\r\n\r\n
+Phone: $phone\r\n\r\n
+Title: $title\r\n\r\n
+Attachment: $attachment_filename\r\n\r\n
+Message:\n$message
 EOT;
 
 require 'PHPMailerAutoload.php';
+
 //Create a new PHPMailer instance
 $mail = new PHPMailer;
 // Set PHPMailer to use the sendmail transport
 $mail->isSendmail();
+$mail->ContentType = 'text/plain';
+$mail->IsHTML(false);
 //Set who the message is to be sent from
-$mail->setFrom('noreply@ucdavis.edu', 'MECHCAP Website');
+$mail->setFrom($from, $from_name);
 //Set an alternative reply-to address
 $mail->addReplyTo($email_address, $name);
 //Set who the message is to be sent to
-$mail->addAddress($to, 'Jason K. Moore');
+$mail->addAddress($to, $to_name);
 //Set the subject line
 $mail->Subject = $email_subject;
 $mail->Body = $email_body;
-$mail->AltBody = $email_body;
 //Attach an image file
 $mail->addAttachment($path_of_uploaded_file);
 //send the message, check for errors
 if (!$mail->send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+
+// Send a confirmation email to submitter.
+
+$conf_email_body = <<<EOT
+Thank you for submitting the proposal:\r\n
+$title\r\n
+We will review the proposal and touch base with you if we have any questions.
+Furthermore, you will receive an email confirming if your proposal is accepted
+for selection by the students. Please feel free to contact us with any
+questions.\r\n\r\n
+Sincerely,\r\n
+Jason K. Moore
+Steven A. Velinsky
+EOT;
+
+//Create a new PHPMailer instance
+$conf_mail = new PHPMailer;
+// Set PHPMailer to use the sendmail transport
+$conf_mail->isSendmail();
+$conf_mail->ContentType = 'text/plain';
+$conf_mail->IsHTML(false);
+$conf_mail->setFrom($to, $to_name);
+$conf_mail->addAddress($email_address, $name);
+$conf_mail->Subject = "Your UCD MECH-CAP proposal has been received.";
+$conf_mail->Body = $conf_email_body;
+if (!$conf_mail->send()) {
+    echo "Mailer Error: " . $conf_mail->ErrorInfo;
 } else {
     echo "Message sent!";
 }
