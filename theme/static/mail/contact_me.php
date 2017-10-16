f7fe8267dc19a134208a1cbea400bab16617f9dd
Jason K. Moore <moorepants@gmail.com>
2017-10-15 20:32:39 -0700

    Better attachment handling.

--- theme/static/mail/contact_me.php
+++ theme/static/mail/contact_me.php
@@ -35,66 +35,75 @@ $url = strip_tags(htmlspecialchars($_POST['url']));
 $title = strip_tags(htmlspecialchars($_POST['title']));
 $message = htmlspecialchars($_POST['message']);
 
-// http://www.html-form-guide.com/email-form/php-email-form-attachment.html
+if (!empty($_FILES['file']['name'])) {
+  // http://www.html-form-guide.com/email-form/php-email-form-attachment.html
 
-//Get the uploaded file information
-$name_of_uploaded_file = basename($_FILES['file']['name']);
+  //Get the uploaded file information
+  $name_of_uploaded_file = basename($_FILES['file']['name']);
 
-//get the file extension of the file
-$type_of_uploaded_file =
-    substr($name_of_uploaded_file,
-    strrpos($name_of_uploaded_file, '.') + 1);
+  //get the file extension of the file
+  $type_of_uploaded_file =
+      substr($name_of_uploaded_file,
+      strrpos($name_of_uploaded_file, '.') + 1);
 
-$size_of_uploaded_file =
-    $_FILES["file"]["size"]/1024;//size in KBs
+  $size_of_uploaded_file =
+      $_FILES["file"]["size"]/1024;//size in KBs
 
-//Settings
-$max_allowed_file_size = 5000; // size in KB
-$allowed_extensions = array("txt", "rst", "md", "pdf", "odt", "doc", "docx", "jpg", "gif", "png");
+  //Settings
+  $max_allowed_file_size = 5000; // size in KB
+  $allowed_extensions = array("txt", "rst", "md", "pdf", "odt", "doc", "docx", "jpg", "gif", "png");
 
-$errors = "";
+  $errors = "";
 
-//Validations
-if($size_of_uploaded_file > $max_allowed_file_size )
-{
-  $errors .= "\n Size of file should be less than $max_allowed_file_size";
-}
-
-//------ Validate the file extension -----
-$allowed_ext = false;
-for($i=0; $i<sizeof($allowed_extensions); $i++)
-{
-  if(strcasecmp($allowed_extensions[$i],$type_of_uploaded_file) == 0)
+  //Validations
+  if($size_of_uploaded_file > $max_allowed_file_size )
   {
-    $allowed_ext = true;
+    $errors .= "\n Size of file should be less than $max_allowed_file_size";
   }
-}
 
-if(!$allowed_ext)
-{
-  $errors .= "\n The uploaded file is not supported file type. ".
-  " Only the following file types are supported: ".implode(',',$allowed_extensions);
-}
-
-//copy the temp. uploaded file to uploads folder
-$upload_folder = "/home/jasonkmoore/moorepants.info/eme185-uploads/";
-if (!file_exists($upload_folder)) {
-    mkdir($upload_folder, 0777, true);
-}
-$shortened_title = substr($title, 0, 10);
-$shortened_org = substr($org, 0, 10);
-$custom_file_name = mb_ereg_replace("([^\w\d\-_\[\]\(\)])", '_',
-  $name."-".$shortened_org."-".$shortened_title);
-$attachment_filename = $custom_file_name.'.'.$type_of_uploaded_file;
-$path_of_uploaded_file = $upload_folder . $attachment_filename;
-$tmp_path = $_FILES["file"]["tmp_name"];
-
-if(is_uploaded_file($tmp_path))
-{
-  if(!copy($tmp_path,$path_of_uploaded_file))
+  //------ Validate the file extension -----
+  $allowed_ext = false;
+  for($i=0; $i<sizeof($allowed_extensions); $i++)
   {
-    $errors .= '\n error while copying the uploaded file';
+    if(strcasecmp($allowed_extensions[$i],$type_of_uploaded_file) == 0)
+    {
+      $allowed_ext = true;
+    }
   }
+
+  if(!$allowed_ext)
+  {
+    $errors .= "\n The uploaded file is not supported file type. ".
+    " Only the following file types are supported: ".implode(',',$allowed_extensions);
+  }
+
+  //copy the temp. uploaded file to uploads folder
+  $upload_folder = "/home/jasonkmoore/moorepants.info/eme185-uploads/";
+  if (!file_exists($upload_folder)) {
+      mkdir($upload_folder, 0777, true);
+  }
+  $shortened_title = substr($title, 0, 10);
+  $shortened_org = substr($org, 0, 10);
+  $custom_file_name = mb_ereg_replace("([^\w\d\-_\[\]\(\)])", '_',
+    $name."-".$shortened_org."-".$shortened_title);
+  $attachment_filename = $custom_file_name.'.'.$type_of_uploaded_file;
+  $path_of_uploaded_file = $upload_folder . $attachment_filename;
+  $tmp_path = $_FILES["file"]["tmp_name"];
+
+  if(is_uploaded_file($tmp_path))
+  {
+    if(!copy($tmp_path,$path_of_uploaded_file))
+    {
+      $errors .= '\n error while copying the uploaded file';
+    }
+  } else {
+    $attachment_filename = "";
+  }
+
+  if ($errors != "") {
+    error_log($errors, 0);
+  }
+
 } else {
   $attachment_filename = "";
 }
@@ -157,8 +166,9 @@ $mail->addAddress($to, $to_name);
 //Set the subject line
 $mail->Subject = $email_subject;
 $mail->Body = $email_body;
-//Attach an image file
-$mail->addAttachment($path_of_uploaded_file);
+if ($attachment_filename != ""){
+  $mail->addAttachment($path_of_uploaded_file);
+}
 //send the message, check for errors
 if (!$mail->send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
@@ -194,10 +204,6 @@ if (!$conf_mail->send()) {
     echo "Mailer Error: " . $conf_mail->ErrorInfo;
 } else {
     echo "Message sent!";
-}
-
-if ($errors != "") {
-  error_log($errors, 0);
 }
 
 error_log("Got to the end of the proposal submission script.", 0);
